19bcfb103f971cf47c78abb45e474e1211645ae9
Casial123 <493959867@qq.com>
2026-03-20 08:43:49 +0800

    Create chatbot_deepseek.py

---
+++ hw02/chatbot_deepseek.py
@@ -0,0 +1,5 @@
+# ===== 配置区域 =====
+API_KEY = "176ad9b9-c4f9-488f-ae35-a326f0042df5"  # 从截图复制的
+BOT_ID = "deepseek-v3-2-251201"                    # 从截图复制的模型ID
+API_URL = "https://ark.cn-beijing.volces.com/api/v3/responses"  # API地址
+# ===================
